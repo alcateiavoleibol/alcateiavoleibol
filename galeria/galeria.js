e63683cf42ea5c76a28d760e877ea445608aa47f
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-14 03:32:23 -0300

    Add files via upload

--- galeria/galeria.js
+++ galeria/galeria.js
@@ -14,10 +14,13 @@ function carregarGaleria() {
     .then(response => response.json())
     .then(data => {
       const galeria = document.getElementById("galeria");
-      data.albuns.forEach(album => {
+      data.albuns.forEach((album, index) => {
         const div = document.createElement("div");
-        div.className = "album";
-        div.innerHTML = `<h3>${album.data}</h3><a href="${album.link}" target="_blank">${album.titulo}</a>`;
+        div.className = "album" + (index === 0 ? " album-destaque" : "");
+        div.innerHTML = `
+          <img src="capa.jpg" alt="Capa do Álbum">
+          <h3>${album.data}</h3>
+          <a href="${album.link}" target="_blank">${album.titulo}</a>`;
         galeria.appendChild(div);
       });
     });
