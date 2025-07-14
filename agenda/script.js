1203f603a2ee4e3328cfc82900509563de559e3e
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-14 04:15:47 -0300

    Delete agenda directory

--- agenda/script.js
+++
@@ -1,8 +0,0 @@
-
-let current = 0;
-const slides = document.querySelectorAll(".slide");
-setInterval(() => {
-  slides[current].classList.remove("active");
-  current = (current + 1) % slides.length;
-  slides[current].classList.add("active");
-}, 3000);
