3b817722d4e0dff904553e40e3937c3d93b46588
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-18 00:25:39 -0300

    Add files via upload

--- projetonew/js/galeria.js
+++ projetonew/js/galeria.js
@@ -4,14 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             const container = document.getElementById("galeria");
             data.albuns.forEach((album, index) => {
-                const capaIndex = (index % 4); // alterna entre 0 a 3
-                const capas = ["capa.jpg", "sd1.jpg", "sd2.jpg", "sd3.jpg"];
-                const capaSelecionada = capas[capaIndex];
+                const capa = album.capa || "capa.jpg";
                 const albumDiv = document.createElement("div");
                 albumDiv.className = "album" + (index === 0 ? " album-destaque" : "");
                 albumDiv.innerHTML = `
                     <a href="${album.link}" target="_blank">
-                        <img src="imagens/${capaSelecionada}" alt="Capa do Álbum">
+                        <img src="imagens/${capa}" alt="Capa do Álbum">
                         <strong>${album.titulo}</strong>
                         <p>${album.data}</p>
                     </a>
