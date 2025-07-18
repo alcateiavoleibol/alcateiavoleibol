d6cb75a1a2af7b173ddc5d911fb1efdeb3148ad8
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-18 17:02:57 -0300

    Update galeria.js

--- projetonew/js/galeria.js
+++ projetonew/js/galeria.js
@@ -36,5 +36,14 @@ function verificarSenha() {
     }
 }
 
+// Controle do menu responsivo
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu-list");
 
+    if (toggle && menu) {
+        toggle.addEventListener("click", () => {
+            menu.classList.toggle("show");
+        });
+    }
 });
