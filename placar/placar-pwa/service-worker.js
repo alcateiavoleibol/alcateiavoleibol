75dc1beb3b428ffed4618356ae146c9eb854b3b7
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-26 19:46:48 -0300

    Add files via upload

--- placar/placar-pwa/service-worker.js
+++ placar/placar-pwa/service-worker.js
@@ -1,27 +1,56 @@
 
-const cacheName = 'placar-v1';
-const assets = [
+const CACHE_NAME = 'placar-alcateia-cache-v1';
+const urlsToCache = [
   './',
   './index.html',
   './placar.css',
   './responsivo-placar.css',
   './placar.js',
-  './manifest.json',
-  './icone.png'
+  './icone.png',
+  './manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(function(networkResponse) {
+          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+            return networkResponse;
+          }
+          let responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+          return networkResponse;
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
