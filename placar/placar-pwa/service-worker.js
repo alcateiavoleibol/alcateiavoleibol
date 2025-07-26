58bb5a5b79bc829a611f60e9471d190d36e85462
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-26 19:17:50 -0300

    Add files via upload

---
+++ placar/placar-pwa/service-worker.js
@@ -0,0 +1,27 @@
+
+const cacheName = 'placar-v1';
+const assets = [
+  './',
+  './index.html',
+  './placar.css',
+  './responsivo-placar.css',
+  './placar.js',
+  './manifest.json',
+  './icone.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
