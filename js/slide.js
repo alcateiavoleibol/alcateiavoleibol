2265c7b43ebf7ee8ab1db9b137c770460bb1b7d0
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-30 06:20:54 -0300

    Delete js directory

--- js/slide.js
+++
@@ -1,7 +0,0 @@
-  let slideIndex = 0;
-  const slides = document.querySelectorAll(".slide img");
-  setInterval(() => {
-    slides[slideIndex].classList.remove("active");
-    slideIndex = (slideIndex + 1) % slides.length;
-    slides[slideIndex].classList.add("active");
-  }, 3000);
