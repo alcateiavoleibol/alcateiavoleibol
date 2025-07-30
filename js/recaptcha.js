00464f748779f058e1dd0d392ea0e0ede196f293
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-07-30 19:49:00 -0300

    Add files via upload

--- js/recaptcha.js
+++ js/recaptcha.js
@@ -5,29 +5,27 @@ document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        const recaptchaResponse = grecaptcha.getResponse();
-        if (!recaptchaResponse) {
-            alert("Por favor, confirme o reCAPTCHA.");
-            return;
-        }
-
-        fetch('/api/verify', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ token: recaptchaResponse })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                enviarFormulario(form);
-            } else {
-                alert("Falha na validação do reCAPTCHA. Tente novamente.");
-                grecaptcha.reset();
-            }
-        })
-        .catch(error => {
-            console.error("Erro:", error);
-            alert("Erro ao validar reCAPTCHA.");
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LeTME4rAAAAAAAORlAzu67I3RVQ3R6_eTC6x0QC', {action: 'submit'}).then(function(token) {
+                // Verificar no backend (API /api/verify)
+                fetch('/api/verify', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ token: token })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        enviarFormulario(form);
+                    } else {
+                        alert("Falha na validação do reCAPTCHA. Tente novamente.");
+                    }
+                })
+                .catch(error => {
+                    console.error("Erro ao validar reCAPTCHA:", error);
+                    alert("Erro ao validar reCAPTCHA.");
+                });
+            });
         });
     });
 
@@ -42,7 +40,6 @@ document.addEventListener('DOMContentLoaded', function () {
             if (response.ok) {
                 successMessage.style.display = "block";
                 form.reset();
-                grecaptcha.reset();
             } else {
                 alert("Erro ao enviar o formulário. Tente novamente.");
             }
